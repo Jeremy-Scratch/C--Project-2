39c628243a546e9666b617700b53685b70691e2d
Scratch <Jeremyscratch6@gmail.com>
2025-08-17 15:24:09 -0400

    desarrollo inicial

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+namespace MiPrimeraApp
+{
+
+    class TaskTracker
+    {
+
+        static void Main(string[] args)
+        {
+            // Escribe una línea de texto en la consola.
+            Console.WriteLine("¡Hola, Mundo!");
+            Console.WriteLine("IM JUST A REGULAR EVERY DAY NORMAL MOTHER FUCKER");
+           
+
+            List<string> tasks = new List<string> { };
+
+            tasks.Add("MMG");
+            Console.WriteLine(tasks[0]);
+             Console.ReadLine();
+
+        }
+    }
+}
---
+++ ConsoleApp1/taskoption.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MiPrimeraApp
+{
+    // Define la clase principal del programa.
+   public class TaskOption
+    {
+        // Este es el método principal que se ejecuta al iniciar el programa.
+      
+    }
+}
